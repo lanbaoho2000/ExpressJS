c639d77567f2125e5c8ea424a4e62db54b1565e3
blanc252023 <h.baolan20022@gmail.com>
2022-07-19 14:33:25 +0700

    Working with Middleware

--- app.js
+++ app.js
@@ -1,57 +1,24 @@
 const express = require("express");
 const app = express();
-const { products, people } = require("./data.js");
+const logger = require("./logger");
+const authorize = require("./authorize");
 
+// app.use -> Use for executing all requests coming
+
+// 1. use vs route
+// 2. options - our own / express / third party
+app.use([authorize, logger]);
+
+// req => middleware => res
 app.get("/", (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+  res.send("Hello World");
 });
 
 app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    console.log({ id, name, image });
-    return { id, name, image };
-  });
-  res.json(newProducts);
+  console.log(req.user);
+  res.send("Products");
 });
 
-// params
-app.get("/api/products/:productID", (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find((product) => {
-    return product.id === Number(productID);
-  });
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exist");
-  }
-  res.json(singleProduct);
-});
-
-// params
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("hello world");
-});
-
-// query string
-app.get("/api/v1/query", (req, res) => {
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    return res.status(202).json({ success: true, data: [] });
-  }
-  res.status(200).json(sortedProducts);
-  res.send("hello world");
-});
 app.listen(5000, () => {
   console.log("Server is running");
 });
